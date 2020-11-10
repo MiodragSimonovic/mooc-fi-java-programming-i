6fb9ef612bed3481f901ce48480acf8cefbccdfd
MiodragSimonovic <Simke@Simke-PC>
2020-11-10 16:37:27 +0100

    Average of two numbers

---
+++ part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class AverageOfTwoNumbers {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        
+        scanner.close();
+        
+        System.out.println("The average is " + (((double) firstNumber + secondNumber) / 2));
+
+    }
+    
+}
