5223661447e64d4b7467967eef601eb863e9dedd
MiodragSimonovic <Simke@Simke-PC>
2020-11-10 17:07:13 +0100

    Adulthood

---
+++ part01-Part01_29.Adulthood/src/main/java/Adulthood.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Adulthood {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How old are you?");
+        int age = Integer.valueOf(scan.nextLine());       
+        
+        scan.close();
+        
+        if (age < 18) {
+            System.out.println("You are not an adult");
+        } else {
+            System.out.println("You are an adult");
+        }
+        
+    }
+    
+}
