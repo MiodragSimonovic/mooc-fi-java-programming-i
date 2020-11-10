1be15b2e8a9c81f0767fa26b726d13784eb0499d
MiodragSimonovic <Simke@Simke-PC>
2020-11-10 16:56:05 +0100

    Ancient

---
+++ part01-Part01_27.Ancient/src/main/java/Ancient.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Ancient {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Give a year: ");
+        int year = Integer.valueOf(scan.nextLine());       
+        
+        scan.close();
+        
+        if (year < 2015) {
+            System.out.println("Ancient history!");
+        }
+        
+    }
+    
+}
